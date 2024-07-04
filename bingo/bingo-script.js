3fcbf85481018f6b79aba034a8ef2a74b23ff963
Quin <88966030+Amtolam@users.noreply.github.com>
2024-07-04 20:30:27 +0000

    Update bingo-script.js

--- bingo/bingo-script.js
+++ bingo/bingo-script.js
@@ -7,7 +7,7 @@ let ndays = 1 + Math.floor(( new Date() - startDay)/(24*3600*1000))
 let dateSum = (a,b) => { return a + Math.floor((ndays + (startDay.getDay()+6-b) % 7) / 7); };
 let dayOfPlayingBingo = [1,2,3,4,5].reduce(dateSum,0);
 
-document.getElementsByTagName("title")[0] += dayOfPlayingBingo;
+document.getElementsByTagName("title")[0].innerHTML += dayOfPlayingBingo;
 
 const numOfCards = 38;
 let images = document.getElementsByTagName("img");
