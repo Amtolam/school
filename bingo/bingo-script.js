1d2866bbc379c821ef753f4146023ba0ca1c324e
Quin <88966030+Amtolam@users.noreply.github.com>
2024-07-04 20:24:31 +0000

    Logic for paly number day change

--- bingo/bingo-script.js
+++ bingo/bingo-script.js
@@ -1,7 +1,21 @@
+// ** DATE LOGIC **
+
+// Count days since start of play (27th June 2024)
+// COPIED FROM: https://stackoverflow.com/questions/25562173/calculate-number-of-specific-weekdays-between-dates
+let startDay = new Date(2024,5,27)
+let ndays = 1 + Math.floor(( new Date() - startDay)/(24*3600*1000))
+let dateSum = (a,b) => { return a + Math.floor((ndays + (startDay.getDay()+6-b) % 7) / 7); };
+let dayOfPlayingBingo = [1,2,3,4,5].reduce(dateSum,0);
+
+document.getElementsByTagName("title")[0] += dayOfPlayingBingo;
+
 const numOfCards = 38;
 let images = document.getElementsByTagName("img");
 
-numArray = Array.from(Array(numOfCards).keys())
+
+
+// ** CARD LOGIC **
+let numArray = Array.from(Array(numOfCards).keys())
 // COPIED FROM https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 let currentIndex = numOfCards;
 // While there remain elements to shuffle...
