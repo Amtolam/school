7297d77f96b2ad024baeb8c36acc7a442db32965
Amtolam <theodorzimmer1@gmail.com>
2024-09-22 14:20:00 +0200

    implemented code duplication for button menus. Testing wether comments get displayed on final page on server

--- ebooks/script.js
+++ ebooks/script.js
@@ -1,5 +1,10 @@
-const buttonBoxes = document.getElementsByClassName("button-Box")
-console.log(buttonBoxes[0].children)
+let buttonBoxes = Array.prototype.slice.call(document.getElementsByClassName("button-box"))
+let buttonBoxHtml = buttonBoxes.splice(0,1)[0]
+
+for (let i = 0; i< buttonBoxes.length; i++){
+    buttonBoxes[i].innerHTML=  buttonBoxHtml.innerHTML
+}
+console.log(buttonBoxes, buttonBoxHtml)
 
 
 
@@ -10,9 +15,4 @@ console.log(buttonBoxes[0].children)
 
 
 
-
-
-
-
-
-
+console.log(1)
