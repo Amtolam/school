481eec8d050153a994a6495a3cf5a2923034f6bc
Amtolam <theodorzimmer1@gmail.com>
2024-09-22 18:32:37 +0200

    figured out 'hidden' attribute and disable func for buttons

--- ebooks/script.js
+++ ebooks/script.js
@@ -1,10 +1,20 @@
+// Copies code from first button-box to all others
 let buttonBoxes = Array.prototype.slice.call(document.getElementsByClassName("button-box"))
-let buttonBoxHtml = buttonBoxes.splice(0,1)[0]
+const buttonBoxHTML = buttonBoxes.splice(0, 1)[0].innerHTML.replace(" disabled=\"\"", "")
 
-for (let i = 0; i< buttonBoxes.length; i++){
-    buttonBoxes[i].innerHTML=  buttonBoxHtml.innerHTML
+for (let i = 0; i < buttonBoxes.length; i++){
+    buttonBoxes[i].innerHTML = buttonBoxHTML
 }
-console.log(buttonBoxes, buttonBoxHtml)
+
+// Disables button that's not needed currently
+for (box of buttonBoxes){
+    let buttonName = box.getAttribute("name")
+    for (let i = 0; i < box.children.length; i++){
+        if (box.children[i].name.includes(buttonName)){
+            box.children[i].setAttribute("disabled", "")
+        }
+    }
+}
 
 
 
