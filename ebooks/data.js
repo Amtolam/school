7798f209fea7e6c01d9aa278e8dc1ad3f0bc3384
ADoorAble <88966030+Amtolam@users.noreply.github.com>
2025-09-21 20:56:30 +0200

    final form of new v1.1 or sth now using Ternary-whatever, but still same functionality as v1 no extra functions yet

--- ebooks/data.js
+++ ebooks/data.js
@@ -3,294 +3,293 @@ data = {
     "Subject": "Mathe",
     "Grade": "13",
     "Cover": "covers/13/mathe.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/fundamente-der-mathematik-schulbuch-13-jahrgangsstufe-9783060428007",
-    "Notes": ""
+    "Link": "https://www.cornelsen.de/produkte/fundamente-der-mathematik-schulbuch-13-jahrgangsstufe-9783060428007",
+    "Notes": "Nur ausgewählte Seiten"
   },
   "Latein 13": {
     "Subject": "Latein",
     "Grade": "13",
     "Cover": "covers/13/latein.jpg",
-    "Clean": "https://www.ccbuchner.de/produkt/lesebuch-latein-oberstufe-3-neu-8750",
-    "Notes": ""
+    "Link": "https://www.ccbuchner.de/produkt/lesebuch-latein-oberstufe-3-neu-8750"
   },
   "Physik 13": {
     "Subject": "Physik",
     "Grade": "13",
     "Cover": "covers/13/physik.jpg",
-    "Clean": "https://www.westermann.de/artikel/978-3-14-152414-7/Dorn-Bader-Physik-SII-Ausgabe-2023-Bayern-Schulbuch-13",
+    "Link": "https://www.westermann.de/artikel/978-3-14-152414-7/Dorn-Bader-Physik-SII-Ausgabe-2023-Bayern-Schulbuch-13",
     "Notes": ""
   },
   "Chemie 13": {
     "Subject": "Chemie",
     "Grade": "13",
     "Cover": "covers/13/chemie.jpg",
-    "Clean": "https://www.ccbuchner.de/produkt/chemie-13-7242",
+    "Link": "https://www.ccbuchner.de/produkt/chemie-13-7242",
     "Notes": ""
   },
   "Biologie 13": {
     "Subject": "Biologie",
     "Grade": "13",
     "Cover": "covers/13/bio.jpg",
-    "Clean": "https://www.westermann.de/artikel/978-3-14-152043-9/Biologie-heute-SII-Ausgabe-2024-Bayern-Schulbuch-13",
+    "Link": "https://www.westermann.de/artikel/978-3-14-152043-9/Biologie-heute-SII-Ausgabe-2024-Bayern-Schulbuch-13",
     "Notes": ""
   },
   "Informatik 13": {
     "Subject": "Informatik",
     "Grade": "13",
     "Cover": "covers/13/info.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/informatik-oldenbourg-formale-sprachen-und-automaten-funktionsweise-eines-rechners-grenzen-der-berechenbarkeit-kuenstliche-intelligenz-band-fuer-das-grundlegende-anforderungsniveau-schulbuch-band-7-grundkurs-9783637029408",    
+    "Link": "https://www.cornelsen.de/produkte/informatik-oldenbourg-formale-sprachen-und-automaten-funktionsweise-eines-rechners-grenzen-der-berechenbarkeit-kuenstliche-intelligenz-band-fuer-das-grundlegende-anforderungsniveau-schulbuch-band-7-grundkurs-9783637029408",    
     "Notes": ""
   },
   "Religion 13 (ev)": {
     "Subject": "Religion (ev)",
     "Grade": "13",
     "Cover": "covers/13/reli.jpg",
-    "Clean": "",
+    "Link": "",
     "Notes": "nicht verfügbar"
   },
   "Ethik 13": {
     "Subject": "Ethik",
     "Grade": "13",
     "Cover": "covers/13/ethik.jpg",
-    "Clean": "https://www.ccbuchner.de/produkt/ethik-in-der-oberstufe-13-8598",
+    "Link": "https://www.ccbuchner.de/produkt/ethik-in-der-oberstufe-13-8598",
     "Notes": ""
   },
   "Geschichte 13": {
     "Subject": "Geschichte",
     "Grade": "13",
     "Cover": "covers/13/geschichte.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/forum-geschichte-neue-ausgabe-schulbuch-13-jahrgangsstufe-9783060662395",
+    "Link": "https://www.cornelsen.de/produkte/forum-geschichte-neue-ausgabe-schulbuch-13-jahrgangsstufe-9783060662395",
     "Notes": ""
   },
   "Politik & Gesellschaft 13": {
     "Subject": "Politik und Gesellschaft",
     "Grade": "13",
     "Cover": "covers/13/pug.jpg",
-    "Clean": "https://www.ccbuchner.de/produkt/politik-aktuell-13-8306",
+    "Link": "https://www.ccbuchner.de/produkt/politik-aktuell-13-8306",
     "Notes": ""
   },
   "Geographie 13": {
     "Subject": "Geographie",
     "Grade": "13",
     "Cover": "covers/13/geo.jpg",
-    "Clean": "https://www.westermann.de/artikel/978-3-14-151952-5/Diercke-Geographie-Ausgabe-2024-fuer-die-Sekundarstufe-II-in-Bayern-Schulbuch-13",
+    "Link": "https://www.westermann.de/artikel/978-3-14-151952-5/Diercke-Geographie-Ausgabe-2024-fuer-die-Sekundarstufe-II-in-Bayern-Schulbuch-13",
     "Notes": ""
   },
   "Deutsch 12/13": {
     "Subject": "Deutsch",
     "Grade": "12, 13",
     "Cover": "covers/12/deutsch.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/deutschbuch-oberstufe-schulbuch-12-13-jahrgangsstufe-9783464630068",
+    "Link": "https://www.cornelsen.de/produkte/deutschbuch-oberstufe-schulbuch-12-13-jahrgangsstufe-9783464630068",
     "Notes": ""
   },
   "Englisch 12/13": {
     "Subject": "Englisch",
     "Grade": "12, 13",
     "Cover": "covers/12/englisch.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/context-schulbuch-oberstufe-9783060345588",
+    "Link": "https://www.cornelsen.de/produkte/context-schulbuch-oberstufe-9783060345588",
     "Notes": ""
   },
   "Französisch 12/13": {
     "Subject": "Französisch",
     "Grade": "12, 13",
     "Cover": "covers/12/franz.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/a-plus-neubearbeitung-schulbuch-9783061231620",
+    "Link": "https://www.cornelsen.de/produkte/a-plus-neubearbeitung-schulbuch-9783061231620",
     "Notes": ""
   },
   "Musik 12/13": {
     "Subject": "Musik",
     "Grade": "12, 13",
     "Cover": "covers/12/musik.jpg",
-    "Clean": "https://www.helbling.com/sites/default/files/media/documents/386227671_Demo_02.pdf",
+    "Link": "https://www.helbling.com/sites/default/files/media/documents/386227671_Demo_02.pdf",
     "Notes": "nur ausgewählte Seiten"
   },
   "Mathe 12": {
     "Subject": "Mathe",
     "Grade": "12",
     "Cover": "covers/12/mathe.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/fundamente-der-mathematik-schulbuch-12-jahrgangsstufe-9783060427956",
+    "Link": "https://www.cornelsen.de/produkte/fundamente-der-mathematik-schulbuch-12-jahrgangsstufe-9783060427956",
     "Notes": "nur ausgewählte Seiten"
   },
   "Mathe VK": {
     "Subject": "Mathe",
     "Grade": "12",
     "Cover": "covers/12/mathevk.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/fundamente-der-mathematik-schulbuch-12-jahrgangsstufe-9783060427345",
+    "Link": "https://www.cornelsen.de/produkte/fundamente-der-mathematik-schulbuch-12-jahrgangsstufe-9783060427345",
     "Notes": "nur ausgewählte Seiten"
   },
   "Latein 12": {
     "Subject": "Latein",
     "Grade": "12",
     "Cover": "covers/12/latein.jpg",
-    "Clean": "https://www.ccbuchner.de/produkt/lesebuch-latein-oberstufe-2-neu-8749",
+    "Link": "https://www.ccbuchner.de/produkt/lesebuch-latein-oberstufe-2-neu-8749",
     "Notes": ""
   },
   "Physik 12": {
     "Subject": "Physik",
     "Grade": "12",
     "Cover": "covers/12/physik.jpg",
-    "Clean": "https://www.westermann.de/artikel/978-3-14-152407-9/Dorn-Bader-Physik-SII-Ausgabe-2023-Bayern-Schulbuch-12",
+    "Link": "https://www.westermann.de/artikel/978-3-14-152407-9/Dorn-Bader-Physik-SII-Ausgabe-2023-Bayern-Schulbuch-12",
     "Notes": ""
   },
   "Chemie 12": {
     "Subject": "Chemie",
     "Grade": "12",
     "Cover": "covers/12/chemie.jpg",
-    "Clean": "https://www.ccbuchner.de/produkt/chemie-12-7241",
+    "Link": "https://www.ccbuchner.de/produkt/chemie-12-7241",
     "Notes": ""
   },
   "Biologie 12": {
     "Subject": "Biologie",
     "Grade": "12",
     "Cover": "covers/12/bio.jpg",
-    "Clean": "https://www.westermann.de/artikel/978-3-14-152035-4/Biologie-heute-SII-Ausgabe-2024-Bayern-Schulbuch-12",
+    "Link": "https://www.westermann.de/artikel/978-3-14-152035-4/Biologie-heute-SII-Ausgabe-2024-Bayern-Schulbuch-12",
     "Notes": ""
   },
   "Informatik 12": {
     "Subject": "Informatik",
     "Grade": "12",
     "Cover": "covers/12/info.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/informatik-oldenbourg-listen-baeume-rekursion-nebenlaeufigkeit-und-threads-informationssicherheit-softwareprojekte-band-fuer-naturwissenschaftlich-technologische-gymnasien-fuer-das-grundlegende-anforderungsniveau-schulbuch-band-6-grundkurs-9783637024762",
+    "Link": "https://www.cornelsen.de/produkte/informatik-oldenbourg-listen-baeume-rekursion-nebenlaeufigkeit-und-threads-informationssicherheit-softwareprojekte-band-fuer-naturwissenschaftlich-technologische-gymnasien-fuer-das-grundlegende-anforderungsniveau-schulbuch-band-6-grundkurs-9783637024762",
     "Notes": ""
   },
   "Religion 12 (ev)": {
     "Subject": "Religion (ev)",
     "Grade": "12",
     "Cover": "covers/12/reli.jpg",
-    "Clean": "https://shop.claudius.de/ortswechselplus-12.html",
+    "Link": "https://shop.claudius.de/ortswechselplus-12.html",
     "Notes": ""
   },
   "Ethik 12": {
     "Subject": "Ethik",
     "Grade": "12",
     "Cover": "covers/12/ethik.jpg",
-    "Clean": "https://www.ccbuchner.de/produkt/ethik-in-der-oberstufe-12-8322",
+    "Link": "https://www.ccbuchner.de/produkt/ethik-in-der-oberstufe-12-8322",
     "Notes": ""
   },
   "Geschichte 12": {
     "Subject": "Geschichte",
     "Grade": "12",
     "Cover": "covers/12/geschichte.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/forum-geschichte-neue-ausgabe-schulbuch-12-jahrgangsstufe-9783060662340",
+    "Link": "https://www.cornelsen.de/produkte/forum-geschichte-neue-ausgabe-schulbuch-12-jahrgangsstufe-9783060662340",
     "Notes": ""
   },
   "Politik & Gesellschaft 12": {
     "Subject": "Politik und Gesellschaft",
     "Grade": "12",
     "Cover": "covers/12/pug.jpg",
-    "Clean": "https://www.ccbuchner.de/produkt/politik-aktuell-12-7709",
+    "Link": "https://www.ccbuchner.de/produkt/politik-aktuell-12-7709",
     "Notes": ""
   },
   "Geographie 12": {
     "Subject": "Geographie",
     "Grade": "12",
     "Cover": "covers/12/geo.jpg",
-    "Clean": "https://www.westermann.de/artikel/978-3-14-151945-7/Diercke-Geographie-Ausgabe-2024-fuer-die-Sekundarstufe-II-in-Bayern-Schulbuch-12",
+    "Link": "https://www.westermann.de/artikel/978-3-14-151945-7/Diercke-Geographie-Ausgabe-2024-fuer-die-Sekundarstufe-II-in-Bayern-Schulbuch-12",
     "Notes": ""
   },
   "Mathe 11": {
     "Subject": "Mathe",
     "Grade": "11",
     "Cover": "covers/11/mathe.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/fundamente-der-mathematik-schulbuch-11-jahrgangsstufe-9783060427901",
+    "Link": "https://www.cornelsen.de/produkte/fundamente-der-mathematik-schulbuch-11-jahrgangsstufe-9783060427901",
     "Notes": ""
   },
   "Deutsch 11": {
     "Subject": "Deutsch",
     "Grade": "11",
     "Cover": "covers/11/deutsch.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/deutschbuch-oberstufe-schulbuch-11-jahrgangsstufe-9783464630006",
+    "Link": "https://www.cornelsen.de/produkte/deutschbuch-oberstufe-schulbuch-11-jahrgangsstufe-9783464630006",
     "Notes": ""
   },
   "Englisch 11": {
     "Subject": "Englisch",
     "Grade": "11",
     "Cover": "covers/11/englisch.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/context-schulbuch-starter-9783060349616",
+    "Link": "https://www.cornelsen.de/produkte/context-schulbuch-starter-9783060349616",
     "Notes": ""
   },
   "Latein 11": {
     "Subject": "Latein",
     "Grade": "11",
     "Cover": "covers/11/latein.jpg",
-    "Clean": "https://www.ccbuchner.de/produkt/lesebuch-latein-oberstufe-1-neu-8748",
+    "Link": "https://www.ccbuchner.de/produkt/lesebuch-latein-oberstufe-1-neu-8748",
     "Notes": ""
   },
   "Französisch 11": {
     "Subject": "Französisch",
     "Grade": "11",
     "Cover": "covers/11/franz.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/a-plus-neubearbeitung-schulbuch-mit-audios-und-videos-charnieres-9783061223663",
+    "Link": "https://www.cornelsen.de/produkte/a-plus-neubearbeitung-schulbuch-mit-audios-und-videos-charnieres-9783061223663",
     "Notes": ""
   },
   "Physik 11": {
     "Subject": "Physik",
     "Grade": "11",
     "Cover": "covers/11/physik.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/duden-physik-schulbuch-11-schuljahr-9783835532700",
+    "Link": "https://www.cornelsen.de/produkte/duden-physik-schulbuch-11-schuljahr-9783835532700",
     "Notes": ""
   },
   "Chemie 11": {
     "Subject": "Chemie",
     "Grade": "11",
     "Cover": "covers/11/chemie.jpg",
-    "Clean": "https://www.ccbuchner.de/produkt/chemie-11-ntg-7240",
+    "Link": "https://www.ccbuchner.de/produkt/chemie-11-ntg-7240",
     "Notes": ""
   },
   "Informatik 11": {
     "Subject": "Informatik",
     "Grade": "11",
     "Cover": "covers/11/info.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/informatik-oldenbourg-graphen-und-codierung-kommunikation-in-netzwerken-kuenstliche-intelligenz-band-fuer-naturwissenschaftlich-technologische-gymnasien-schulbuch-band-5-9783637024731",
+    "Link": "https://www.cornelsen.de/produkte/informatik-oldenbourg-graphen-und-codierung-kommunikation-in-netzwerken-kuenstliche-intelligenz-band-fuer-naturwissenschaftlich-technologische-gymnasien-schulbuch-band-5-9783637024731",
     "Notes": ""
   },
   "Religion 11 (ev)": {
     "Subject": "Religion (ev)",
     "Grade": "11",
     "Cover": "covers/11/reli.jpg",
-    "Clean": "https://shop.claudius.de/ortswechselplus-11.html",
+    "Link": "https://shop.claudius.de/ortswechselplus-11.html",
     "Notes": ""
   },
   "Ethik 11": {
     "Subject": "Ethik",
     "Grade": "11",
     "Cover": "covers/11/ethik.jpg",
-    "Clean": "https://www.ccbuchner.de/produkt/ethik-in-der-oberstufe-11-8320",
+    "Link": "https://www.ccbuchner.de/produkt/ethik-in-der-oberstufe-11-8320",
     "Notes": ""
   },
   "Geschichte 11": {
     "Subject": "Geschichte",
     "Grade": "11",
     "Cover": "covers/11/geschichte.jpg",
-    "Clean": "https://www.cornelsen.de/produkte/forum-geschichte-neue-ausgabe-schulbuch-11-jahrgangsstufe-9783060662296",
+    "Link": "https://www.cornelsen.de/produkte/forum-geschichte-neue-ausgabe-schulbuch-11-jahrgangsstufe-9783060662296",
     "Notes": ""
   },
   "Politik & Gesellschaft 11": {
     "Subject": "Politik und Gesellschaft",
     "Grade": "11",
     "Cover": "covers/11/pug.jpg",
-    "Clean": "https://www.ccbuchner.de/produkt/politik-aktuell-11-7708",
+    "Link": "https://www.ccbuchner.de/produkt/politik-aktuell-11-7708",
     "Notes": ""
   },
   "Musik 11": {
     "Subject": "Musik",
     "Grade": "11",
     "Cover": "covers/11/musik.jpg",
-    "Clean": "https://www.helbling.com/sites/default/files/media/documents/386227575_Demo.pdf",
+    "Link": "https://www.helbling.com/sites/default/files/media/documents/386227575_Demo.pdf",
     "Notes": "nur ausgewählte Seiten"
   },
   "Geographie 11": {
     "Subject": "Geographie",
     "Grade": "11",
     "Cover": "covers/11/geo.jpg",
-    "Clean": "https://www.westermann.de/artikel/978-3-507-53296-0/Seydlitz-Geographie-Aktuelle-Ausgabe-fuer-Gymnasien-in-Bayern-Schulbuch-11",
+    "Link": "https://www.westermann.de/artikel/978-3-507-53296-0/Seydlitz-Geographie-Aktuelle-Ausgabe-fuer-Gymnasien-in-Bayern-Schulbuch-11",
     "Notes": ""
   },
   "Atlas": {
     "Subject": "Geographie",
     "Grade": "5,6,7,8,9,10,11,12,13",
     "Cover": "covers/misc/atlas.jpg",
-    "Clean": "https://www.westermann.de/artikel/978-3-14-100902-6/Diercke-Weltatlas-Aktuelle-Ausgabe-fuer-Bayern",
+    "Link": "https://www.westermann.de/artikel/978-3-14-100902-6/Diercke-Weltatlas-Aktuelle-Ausgabe-fuer-Bayern",
     "Notes": ""
   }
 }
--- ebooks/script.js
+++ ebooks/script.js
@@ -1,18 +1,20 @@
-let bookWrappersDict = {
-    "5":document.querySelector(".books-5"),
-    "6":document.querySelector(".books-6"),
-    "7":document.querySelector(".books-7"),
-    "8":document.querySelector(".books-8"),
-    "9":document.querySelector(".books-9"),
-    "10":document.querySelector(".books-10"),
-    "11":document.querySelector(".books-11"),
-    "12":document.querySelector(".books-12"),
-    "13":document.querySelector(".books-13"),
-    "misc":document.querySelector(".books-misc"),
-    "all":document.querySelector(".books-all"),
+const bookWrappersDict = {
+    "5": document.querySelector(".books-5"),
+    "6": document.querySelector(".books-6"),
+    "7": document.querySelector(".books-7"),
+    "8": document.querySelector(".books-8"),
+    "9": document.querySelector(".books-9"),
+    "10": document.querySelector(".books-10"),
+    "11": document.querySelector(".books-11"),
+    "12": document.querySelector(".books-12"),
+    "13": document.querySelector(".books-13"),
+    "misc": document.querySelector(".books-misc"),
+    "all": document.querySelector(".books-all")
 }
-let curSeries = bookWrappersDict["all"]
 let curButton = document.querySelector("button:disabled")
+//console.log(document.querySelectorAll("button"))
+let curSeries = document.querySelector(`.${curButton.name}`)
+curSeries.classList.toggle("hidden")
 // Button structre
 const changeBookSeries = (event) => {
     curButton.toggleAttribute("disabled")
@@ -21,7 +23,6 @@ const changeBookSeries = (event) => {
     curSeries.classList.toggle("hidden")
     curSeries = document.querySelector(`.book-wrapper.${curButton.name}`)
     curSeries.classList.toggle("hidden")
-    console.log(event)
 }
 
 const bookChoiceButtons = document.getElementsByClassName("book-choice")
@@ -29,37 +30,52 @@ for (let i = 0; i < bookChoiceButtons.length; i++) {
     bookChoiceButtons[i].addEventListener("click", changeBookSeries)
 }
 
+
+const bookOrderBySubject = {
+    "Mathe": -1,
+    "Deutsch": 1,
+    "Englisch": 2,
+    "Latein": 3,
+    "Französisch": 4,
+    "Biologie": 5,
+    "Chemie": 6,
+    "Physik": 7,
+    "Informatik": 8,
+    "Musik": 9,
+    "Religion (ev)": 10,
+    "Ethik": 11,
+    "Geschichte": 12,
+    "Politik und Gesellschaft": 13,
+    "Geographie": 14
+}
+
+let curOrder
+
 for (let book in data) {
-    if (data[book].Notes) {
-        htmlString =
-            `<div class="book" title="${book}">
-                <a href="${data[book].Clean}">
-                    <div class="overlay-box">
-                        <div class="notes">${data[book].Notes}</div>
-                    </div>
-                    <img width=240px class="book-img" alt="loading... ${data[book].Subject}" src="${data[book].Cover}">
-                </a>
+    curOrder = bookOrderBySubject[data[book].Subject]
+    htmlString =
+        `<div class="book" title="${book}" style="order:${curOrder ? curOrder : 99}">
+        ${data[book].Link ?
+            `<a href="${data[book].Link}">`
+        : ""} 
+        ${data[book].Notes ?
+            `<div class="overlay-box">
+                <div class="notes">${data[book].Notes}</div>
             </div>`
-        if (!data[book].Clean) {
-            htmlString =
-                `<div class="book" title="${book}">
-                    <div class="overlay-box">
-                        <div class="notes">${data[book].Notes}</div>
-                    </div>
-                        
-                    <img width=240px class="book-img" alt="loading... ${book}" src="${data[book].Cover}">
-                </div>`
+        : ""} 
+            <img loading="lazy" width=240px class="book-img" alt="loading... ${book}" src="${data[book].Cover}">
+        ${data[book].Clean ?
+            "</a>"
+        : ""}
+        </div>`
+
+    for (grade of data[book].Grade.split(",")) {
+        curWrap = bookWrappersDict[Number(grade)]
+        //console.log(curWrap, curSeries, curWrap==curSeries)
+        if (curWrap == curSeries) {
+            htmlString = htmlString.replace('loading="lazy"', "")
         }
-    } else {
-        htmlString =
-            `<div class="book" title="${book}">
-			<a href="${data[book].Clean}">
-                <img width=240px class="book-img" alt="loading... ${book}" src="${data[book].Cover}">
-			</a>
-		</div>`
-    }
-    for (grade of data[book].Grade.split(",")){
-        bookWrappersDict[Number(grade)].innerHTML += htmlString
+        curWrap.innerHTML += htmlString
     }
     bookWrappersDict["all"].innerHTML += htmlString
 }
